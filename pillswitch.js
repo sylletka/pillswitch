e1664b2a3b6577c272a44d602f4a87493a700bd9
sylletka <sylletka@users.noreply.github.com>
2014-07-03 14:40:51 +0200

    Corrected value

--- pillswitch.js
+++ pillswitch.js
@@ -10,15 +10,13 @@
 				'display' : 'none'
 			}).addClass('switchValue');
 			checkbox.wrap("<span class='pillSwitch'></span>");
-			var status = checkbox.is(':checked');
+			var status = checkbox.prop('checked');
 			if (status) {
 				checkbox.after("<span class='off inactive'>" + settings.no + "</span>");
 				checkbox.after("<span class='on active'>" + settings.yes + "</span>");
-				checkbox.val(1);
 			} else {
 				checkbox.after("<span class='off active'>" + settings.no + "</span>");
 				checkbox.after("<span class='on inactive'>" + settings.yes + "</span>");
-				checkbox.val(0);
 			}
 			var width = Math.max(checkbox.siblings('.on').width(), checkbox.siblings('.off').width());
 			checkbox.siblings('span').each(function () {
@@ -26,18 +24,18 @@
 					'width' : width
 				});
 			});
-			$('.pillSwitch').click(function () {
-				var pillSwitch = $(this);
-				var checkbox = pillSwitch.find("input[type='checkbox'].switchValue");
-				pillSwitch.find('.off').toggleClass('active').toggleClass('inactive');
-				pillSwitch.find('.on').toggleClass('active').toggleClass('inactive');
-				var status = checkbox.prop('checked');
-				if (status == true) {
-					checkbox.prop('checked', false);
-				} else {
-					checkbox.prop('checked', true);
-				}
-			});
+		});
+		$('body').on('click','.pillSwitch',function(){
+			var pillSwitch = $(this);
+			var checkbox = pillSwitch.find("input[type='checkbox'].switchValue");
+			pillSwitch.find('.off').toggleClass('active').toggleClass('inactive');
+			pillSwitch.find('.on').toggleClass('active').toggleClass('inactive');
+			var status = checkbox.prop('checked');
+			if (status == true) {
+				checkbox.prop('checked', false).removeAttr('checked');
+			} else {
+				checkbox.prop('checked', true).attr({'checked' : 'checked'});
+			}
 		});
 		return this;
 	};
